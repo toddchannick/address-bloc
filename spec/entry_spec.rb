7e96eb34171869a974c09fa19a5e6a7fc21b7dbf
Todd Channick <toc5012@gmail.com>
2015-09-27 18:34:09 -0400

    Checkpoint 22 Completed - Address Bloc: Menu

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -19,5 +19,13 @@ RSpec.describe Entry do  #file is a spec file which tests Entry class
       expect(entry).to respond_to(:email)
     end
 
+    describe "#to_s" do
+     it "prints an entry as a string" do
+       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
+
+       expect(entry.to_s).to eq(expected_string)
+     end
+
   end
 end

a4a8d7433cc73a5ad00aaf50dcb375659554ef15
Todd Channick <toc5012@gmail.com>
2015-09-28 22:55:55 -0400

    Checkpoint 25 Completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -15,7 +15,8 @@
      puts "3 - Create an entry"
      puts "4 - Search for an entry"
      puts "5 - Import entries from a CSV"
-     puts "6 - Exit"
+     puts "6 - Delete all entries"
+     puts "7 - Exit"
      print "Enter your selection: "
 
      selection = gets.to_i
@@ -41,6 +42,10 @@
        read_csv
        main_menu
      when 6
+       system "clear"
+       detonate
+       main_menu
+     when 7
        puts "Good-bye!"
        exit(0)
 
@@ -103,10 +108,110 @@
      entry_submenu(entry)
    end
 
-   def search_entries
+   def read_csv
+     print "Enter CSV file to import: "
+     file_name = gets.chomp
+
+     if file_name.empty?
+       system "clear"
+       puts "No CSV file read"
+       main_menu
+     end
+
+     begin
+       entry_count = @address_book.import_from_csv(file_name).count
+       system "clear"
+       puts "#{entry_count} new entries added from #{file_name}"
+     rescue
+       puts "#{file_name} is not a valid CSV file, please enter the name of a valid CSV file"
+       read_csv
+     end
    end
 
-   def read_csv
+   def delete_entry(entry)
+     @address_book.entries.delete(entry)
+     system "clear"
+     puts "#{entry.name} has been deleted"
+   end
+
+   def edit_entry(entry)
+
+     print "Updated name: "
+     name = gets.chomp
+     print "Updated phone number: "
+     phone_number = gets.chomp
+     print "Updated email: "
+     email = gets.chomp
+
+     entry.name = name if !name.empty?
+     entry.phone_number = phone_number if !phone_number.empty?
+     entry.email = email if !email.empty?
+     system "clear"
+
+     puts "Updated entry:"
+     puts entry
+   end
+
+   def search_entries
+     print "Search by name: "
+     name = gets.chomp
+
+     match = @address_book.binary_search(name)
+     system "clear"
+
+     if match
+       puts match.to_s
+       search_submenu(match)
+     else
+       puts "No match found for #{name}"
+     end
+   end
+
+   def search_submenu(entry)
+
+     puts "\nd - delete entry"
+     puts "e - edit this entry"
+     puts "m - return to main menu"
+
+     selection = gets.chomp
+
+     case selection
+     when "d"
+       system "clear"
+       delete_entry(entry)
+       main_menu
+     when "e"
+       edit_entry(entry)
+       system "clear"
+       main_menu
+     when "m"
+       system "clear"
+       main_menu
+     else
+       system "clear"
+       puts "#{selection} is not a valid input"
+       puts entry.to_s
+       search_submenu(entry)
+     end
+   end
+
+   def detonate
+     if @address_book.entries.empty?
+       puts "Address book is empty"
+       main_menu
+     end
+
+     puts "Are you sure you want to delete all? (y/n)"
+     input = gets.chomp
+     input.downcase!
+
+     if input == 'y'
+       @address_book.entries.clear
+       system "clear"
+       puts "All entries have been deleted"
+     else
+       main_menu
+     end
    end
 
    def entry_submenu(entry)
@@ -121,14 +226,17 @@
      case selection
        when "n"
        when "d"
+         delete_entry(entry)
        when "e"
+         edit_entry(entry)
+         entry_submenu(entry)
        when "m"
          system "clear"
          main_menu
        else
          system "clear"
          puts "#{selection} is not a valid input"
-         entries_submenu(entry)
+         entry_submenu(entry)
      end
    end
 
